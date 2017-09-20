8315275640ed7a45cb20ea9910c11ed04ab1bef6
Deanna Lepke <deannalepke@gmail.com>
2017-09-20 20:09:29 +0000

    added form submission functionality, and ajax for fetching tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,38 @@
 
 $(function() {
 
+  $(".submit").on("click", function() {
+    event.preventDefault();
+    var tweet = $(".tweet-text").val();
+    if (tweet.length > 140) {
+      $("p.color-warning").removeClass("hide").text("Exceeded max character count.");
+    } else if (tweet === '') {
+      $("p.color-warning").removeClass("hide").text("Please enter some text.");
+    } else {
+      $("p.color-warning").addClass("hide");
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: {
+          text: tweet
+        },
+        success: function() {
+          console.log("successful post!");
+          // loadTweets();
+          $(".tweet-text").val('');
+        }
+      })
+    }
+  });
+
+  function loadTweets() {
+    $.getJSON('/tweets', function(response) {
+      renderTweets(response);
+    })
+  };
+
+  loadTweets();
+
   function createTweetElement(tweetObject) {
     var tweetInProgress = $("<article>").addClass("tweet");
 
@@ -36,54 +68,8 @@ $(function() {
     }
   }
 
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
 
-renderTweets(data);
+// renderTweets(data);
 
 });
 
